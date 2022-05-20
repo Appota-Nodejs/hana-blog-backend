2e48c10c87d637007050659a980773009dc38fe5
DaLast1 <dalast1@gmail.com>
2022-05-20 15:49:20 +0700

    update login with metamask

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,15 +6,6 @@ const userControllers = require('../controllers/user-controllers');
 const router = express.Router();
 
 router.post(
-  '/login',
-  [
-    check('username').not().isEmpty().trim().escape(),
-    check('password').trim().isLength({ min: 6 }).escape(),
-  ],
-  userControllers.login
-);
-
-router.post(
   '/register',
   [
     check('username').not().isEmpty().trim().escape(),
@@ -24,14 +15,23 @@ router.post(
   userControllers.register
 );
 
+router.post(
+  '/login',
+  [
+    check('username').not().isEmpty().trim().escape(),
+    check('password').trim().isLength({ min: 6 }).escape(),
+  ],
+  userControllers.login
+);
+
 router.get('/:publicAddress', userControllers.getPublicAddress);
 
 router.post(
   '/metamask-login',
-  [
-    check('signature').not().isEmpty().trim(),
-    check('publicAddress ').not().isEmpty().trim(),
-  ],
+  // [
+  //   check('signature').not().isEmpty().trim(),
+  //   check('publicAddress ').not().isEmpty().trim(),
+  // ],
   userControllers.metamaskLogin
 );
 
