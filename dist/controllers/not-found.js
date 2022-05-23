04db38c8d3322d1a2dc1781d6b149e9b74862b70
DaLast1 <dalast1@gmail.com>
2022-05-23 17:09:05 +0700

    ignore dist folder

--- dist/controllers/not-found.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var get404 = function (req, res, next) {
-    res.status(404).json({ message: 'Page not found' });
-};
-exports.default = get404;
--- dist/utils/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_1 = require("sequelize");
-var sequelize = new sequelize_1.Sequelize('mysql://root:MySQL.21@localhost:3306/hana-blog');
-exports.default = sequelize;
