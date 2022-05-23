3e96356ef54ba555b1b5d77882795bdc4e0b3a61
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-05-23 17:25:03 +0700

    feature post convert to typescript

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -1,40 +1,56 @@
-const { validationResult } = require('express-validator');
+import { validationResult, Result, ValidationError } from 'express-validator';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 const Post = require('../models/post');
 const isValidInput = require('../utils/input-validator');
 
+// interface for object data
+interface RequestData {
+  title: string;
+  content: string;
+  imageLink: string;
+}
+
 // Get all the posts - list
-exports.posts = async (req, res, next) => {
+const posts: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const posts = await Post.findAll();
+    const posts: Array<object> = await Post.findAll();
 
     return res.status(200).json({
       success: true,
       message: 'Get all the posts',
-      posts,
+      posts
     });
   } catch (error) {
-    next(new Error('Error Server!'));
+    return next(new Error('Error Server!'));
   }
 };
 
 // get one the post
-exports.getOne = async (req, res, next) => {
+const getOne: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const id = req.params.id;
+    const id: number = Number(req.params.id);
     if (!isValidInput(id, 'id')) {
-      const error = new Error('Invalid inputs, please use valid inputs');
+      const error: Error = new Error('Invalid inputs, please use valid inputs');
       return next(error);
     }
 
-    const post = await Post.findOne({
-      where: { id },
+    const post: object = await Post.findOne({
+      where: { id }
     });
 
     return res.status(200).json({
       success: true,
       message: 'Get one the post',
-      post,
+      post
     });
   } catch (error) {
     next(new Error('Error Server!'));
@@ -42,38 +58,44 @@ exports.getOne = async (req, res, next) => {
 };
 
 // create one the post
-exports.create = async (req, res, next) => {
+const create: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const validationError = validationResult(req);
+    const validationError: Result<ValidationError> = validationResult(req);
     if (!validationError.isEmpty()) {
-      const error = new Error(
+      const error: Error = new Error(
         'Invalid post, please check your inputs and try again later'
       );
       return next(error);
     }
 
-    const { title, content, imageLink } = req.body;
-    const authorId = req.userData.userId;
+    const body: RequestData = req.body;
+    const { title, content, imageLink } = body;
+    const authorId: number = res.locals.userId;
+
     if (
       !isValidInput(title, 'text') ||
       !isValidInput(content, 'text') ||
       !isValidInput(authorId, 'id')
     ) {
-      const error = new Error('Invalid inputs, please use valid inputs');
+      const error: Error = new Error('Invalid inputs, please use valid inputs');
       return next(error);
     }
 
-    const post = await Post.create({
-      title,
-      content,
-      imageLink,
-      authorId,
+    const post: object = await Post.create({
+      title: title,
+      content: content,
+      imageLink: imageLink,
+      authorId: authorId
     });
 
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: 'Create the post success',
-      post,
+      post
     });
   } catch (error) {
     next(new Error('Error Server!'));
@@ -81,34 +103,45 @@ exports.create = async (req, res, next) => {
 };
 
 // update one the post
-exports.update = async (req, res, next) => {
+const update: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const validationError = validationResult(req);
+    const validationError: Result<ValidationError> = validationResult(req);
     if (!validationError.isEmpty()) {
-      const error = new Error(
+      const error: Error = new Error(
         'Invalid post, please check your inputs and try again later'
       );
       return next(error);
     }
 
-    const body = req.body;
-    const id = req.params.id;
+    const body: RequestData = req.body;
+    const { title, content, imageLink } = body;
+    const id: number = Number(req.params.id);
+
     if (!isValidInput(id, 'id')) {
-      const error = new Error('Invalid inputs, please use valid inputs');
+      const error: Error = new Error('Invalid inputs, please use valid inputs');
       return next(error);
     }
 
-    body['authorId'] = req.userData.userId;
-
-    const post = await Post.update(body, {
-      where: { id },
-    });
+    const post: Array<number> = await Post.update(
+      {
+        title: title,
+        content: content,
+        imageLink: imageLink
+      },
+      {
+        where: { id: id, authorId: res.locals.userId }
+      }
+    );
 
     if (!post[0]) throw next(new Error('ID post is not found!'));
 
     res.status(200).json({
       success: true,
-      message: 'Update the post success',
+      message: 'Update the post success'
     });
   } catch (error) {
     next(new Error('Error Server!'));
@@ -116,25 +149,31 @@ exports.update = async (req, res, next) => {
 };
 
 // delete one the post
-exports.destroy = async (req, res, next) => {
+const destroy: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const id = req.params.id;
+    const id: number = Number(req.params.id);
     if (!isValidInput(id, 'id')) {
-      const error = new Error('Invalid inputs, please use valid inputs');
+      const error: Error = new Error('Invalid inputs, please use valid inputs');
       return next(error);
     }
 
-    const post = await Post.destroy({
-      where: { id },
+    const post: number = await Post.destroy({
+      where: { id: id, authorId: res.locals.userId }
     });
-  
+
     if (!post) throw next(new Error('ID post is not found!'));
 
     res.status(200).json({
       success: true,
-      message: 'Delete the post success',
+      message: 'Delete the post success'
     });
   } catch (error) {
     next(new Error('Error Server!'));
   }
 };
+
+export { posts, getOne, create, update, destroy };
